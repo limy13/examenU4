3f24d9528f028bcff80dffa1bde1f592823e0945
lizeth <l23121123@morelia.tecnm.mx>
2024-06-03 16:34:44 -0600

    Se subieron cambios

---
+++ UsuarioModel.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UsuarioModel {
+
+    private HashMap<Rol, ArrayList<Usuario>> ISC;
+    private HashMap<Rol, ArrayList<Usuario>> IMAT;
+    private HashMap<Rol, ArrayList<Usuario>> ELC;
+    private ArrayList<Profesor> PROFESOR;
+    private ArrayList<Alumno> ALUMNO;
+    private ArrayList<Coordinador> COORDINADOR;
+
+    public HashMap<Rol, ArrayList<Usuario>> getISC() {
+        return ISC;
+    }
+
+    public HashMap<Rol, ArrayList<Usuario>> getIMAT() {
+        return IMAT;
+    }
+
+    public HashMap<Rol, ArrayList<Usuario>> getELC() {
+        return ELC;
+    }
+
+    public ArrayList<Profesor> getPROFESOR() {
+        return PROFESOR;
+    }
+
+    public ArrayList<Alumno> getALUMNO() {
+        return ALUMNO;
+    }
+
+    public ArrayList<Coordinador> getCOORDINADOR() {
+        return COORDINADOR;
+    }
+
+}
---
+++ UsuarioSerializer.java
@@ -0,0 +1,20 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public abstract class UsuarioSerializer {
+
+    public static void serialize() {
+        Gson json = new GsonBuilder().setPrettyPrinting().create();
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("usuarios.json"));
+            json.toJson(Sistema.usuarios, writer);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
