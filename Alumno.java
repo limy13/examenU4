0c93af4b19b67c8c5f1512eb2a1c1661fdfc93d0
lizeth <l23121123@morelia.tecnm.mx>
2024-06-03 23:11:18 -0600

    Se subieron cambios

--- Alumno.java
+++ Alumno.java
@@ -19,17 +19,28 @@ public class Alumno extends Usuario {
     }
 
     private void asignarMaterias(Carreras carrera, int semestre) {
-        String[] materiasSemestre = Sistema.obtenerMaterias(carrera, semestre);
-        for (String nombreMateria : materiasSemestre) {
-            materias.add(new Materia(nombreMateria));
+        // Aquí puse un try
+        try {
+            String[] materiasSemestre = Sistema.obtenerMaterias(carrera, semestre);
+            for (String nombreMateria : materiasSemestre) {
+                materias.add(new Materia(nombreMateria));
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al asignar materias. Por favor, intente de nuevo.");
         }
     }
 
-    public ArrayList<Materia> getMaterias() { return materias;}
+    public ArrayList<Materia> getMaterias() {
+        return materias;
+    }
 
-    public ArrayList<Materia> getMateriasInscritas() { return materiasInscritas; }
+    public ArrayList<Materia> getMateriasInscritas() {
+        return materiasInscritas;
+    }
 
-    public void setMateriasInscritas(ArrayList<Materia> materiasInscritas) {this.materiasInscritas = materiasInscritas;}
+    public void setMateriasInscritas(ArrayList<Materia> materiasInscritas) {
+        this.materiasInscritas = materiasInscritas;
+    }
 
     @Override
     public void verInformacionPersonal() {
@@ -40,80 +51,90 @@ public class Alumno extends Usuario {
     }
 
     public static void registrarAlumno() {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> datosComun = Usuario.datosComun(Rol.ALUMNO);
-        String nombre = datosComun.get(0);
-        String apellidos = datosComun.get(1);
-        String fechaNacimiento = datosComun.get(2);
-        String estado = datosComun.get(3);
-        String ciudad = datosComun.get(4);
-        String direccion = datosComun.get(5);
-        String curp = datosComun.get(6);
-        String nombreUsuario = datosComun.get(7);
-        String contrasena = datosComun.get(8);
-        String numeroControl = datosComun.get(9);
+        // Aquí puse un try
+        try {
+            Scanner scanner = new Scanner(System.in);
+            ArrayList<String> datosComun = Usuario.datosComun(Rol.ALUMNO);
+            String nombre = datosComun.get(0);
+            String apellidos = datosComun.get(1);
+            String fechaNacimiento = datosComun.get(2);
+            String estado = datosComun.get(3);
+            String ciudad = datosComun.get(4);
+            String direccion = datosComun.get(5);
+            String curp = datosComun.get(6);
+            String nombreUsuario = datosComun.get(7);
+            String contrasena = datosComun.get(8);
+            String numeroControl = datosComun.get(9);
 
-        System.out.print("Ingrese el semestre del alumno: ");
-        int semestre = scanner.nextInt();
-        scanner.nextLine(); // Consumir la nueva línea después de nextInt()
+            System.out.print("Ingrese el semestre del alumno: ");
+            int semestre = scanner.nextInt();
+            scanner.nextLine(); // Consumir la nueva línea después de nextInt()
 
-        Alumno alumno = new Alumno(nombre, apellidos, ciudad, estado, curp.toUpperCase(), direccion, numeroControl, fechaNacimiento, Sistema.carrera, semestre, nombreUsuario, contrasena);
+            Alumno alumno = new Alumno(nombre, apellidos, ciudad, estado, curp.toUpperCase(), direccion, numeroControl, fechaNacimiento, Sistema.carrera, semestre, nombreUsuario, contrasena);
 
-        // Asegúrate de que la lista de alumnos para la carrera actual no sea null
-        if (!Sistema.usuarios.containsKey(Sistema.carrera)) {
-            Sistema.usuarios.put(Sistema.carrera, new HashMap<>());
+            // Asegúrate de que la lista de alumnos para la carrera actual no sea null
+            if (!Sistema.usuarios.containsKey(Sistema.carrera)) {
+                Sistema.usuarios.put(Sistema.carrera, new HashMap<>());
+            }
+            if (!Sistema.usuarios.get(Sistema.carrera).containsKey(Rol.ALUMNO)) {
+                Sistema.usuarios.get(Sistema.carrera).put(Rol.ALUMNO, new ArrayList<>());
+            }
+
+            // Agrega el alumno a la lista correspondiente
+            Sistema.usuarios.get(Sistema.carrera).get(Rol.ALUMNO).add(alumno);
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al registrar al alumno. Por favor, intente de nuevo.");
         }
-        if (!Sistema.usuarios.get(Sistema.carrera).containsKey(Rol.ALUMNO)) {
-            Sistema.usuarios.get(Sistema.carrera).put(Rol.ALUMNO, new ArrayList<>());
-        }
-
-        // Agrega el alumno a la lista correspondiente
-        Sistema.usuarios.get(Sistema.carrera).get(Rol.ALUMNO).add(alumno);
     }
 
     public static void modificarAlumno() {
         boolean band = false;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\n---- Modificar alumno ----");
-        System.out.print("Ingrese el número de control del alumno que desea modificar: ");
-        String numero = scanner.nextLine();
-        for (Usuario usuario : Sistema.usuarios.get(Sistema.carrera).get(Rol.ALUMNO)) {
-            if (numero.equals(usuario.getNumeroControl())) {
-                band = true;
-                System.out.print("\nNombre: ");
-                String nombre = scanner.nextLine();
-                usuario.setNombre(nombre);
-                System.out.print("Apellido paterno: ");
-                String apellidoP = scanner.nextLine();
-                System.out.print("Apellido materno: ");
-                String apellidoM = scanner.nextLine();
-                usuario.setApellido(apellidoP.concat(" ").concat(apellidoM));
-                String fechaNacimiento = validarFecha(); // Método para validar fecha
-                usuario.setFechaNacimiento(fechaNacimiento);
-                System.out.print("Género (H/M): ");
-                String genero = scanner.nextLine().toUpperCase();
-                System.out.print("Estado: ");
-                String estado = validarEstado(scanner.nextLine());
-                usuario.setEstado(estado);
-                System.out.print("Ciudad: ");
-                String ciudad = scanner.nextLine();
-                usuario.setCiudad(ciudad);
-                System.out.print("Dirección: ");
-                String direccion = scanner.nextLine();
-                usuario.setDireccion(direccion);
-                String nombreUsuario = registrarNombreUsuario();
-                usuario.setNombreUsuario(nombreUsuario);
-                String numeroControl = generarNumeroControl(nombre.charAt(0), Rol.ALUMNO);
-                usuario.setNumeroControl(numeroControl);
-                System.out.print("Contraseña: ");
-                String contrasena = scanner.nextLine();
-                usuario.setContrasena(contrasena);
-                String curp = generarCurp(nombre, apellidoP, apellidoM, fechaNacimiento, genero, estado);
-                usuario.setCurp(curp);
+        // Aquí puse un try
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\n---- Modificar alumno ----");
+            System.out.print("Ingrese el número de control del alumno que desea modificar: ");
+            String numero = scanner.nextLine();
+            for (Usuario usuario : Sistema.usuarios.get(Sistema.carrera).get(Rol.ALUMNO)) {
+                if (numero.equals(usuario.getNumeroControl())) {
+                    band = true;
+                    System.out.print("\nNombre: ");
+                    String nombre = scanner.nextLine();
+                    usuario.setNombre(nombre);
+                    System.out.print("Apellido paterno: ");
+                    String apellidoP = scanner.nextLine();
+                    System.out.print("Apellido materno: ");
+                    String apellidoM = scanner.nextLine();
+                    usuario.setApellido(apellidoP.concat(" ").concat(apellidoM));
+                    String fechaNacimiento = validarFecha(); // Método para validar fecha
+                    usuario.setFechaNacimiento(fechaNacimiento);
+                    System.out.print("Género (H/M): ");
+                    String genero = scanner.nextLine().toUpperCase();
+                    System.out.print("Estado: ");
+                    String estado = validarEstado(scanner.nextLine());
+                    usuario.setEstado(estado);
+                    System.out.print("Ciudad: ");
+                    String ciudad = scanner.nextLine();
+                    usuario.setCiudad(ciudad);
+                    System.out.print("Dirección: ");
+                    String direccion = scanner.nextLine();
+                    usuario.setDireccion(direccion);
+                    String nombreUsuario = registrarNombreUsuario();
+                    usuario.setNombreUsuario(nombreUsuario);
+                    String numeroControl = generarNumeroControl(nombre.charAt(0), Rol.ALUMNO);
+                    usuario.setNumeroControl(numeroControl);
+                    System.out.print("Contraseña: ");
+                    String contrasena = scanner.nextLine();
+                    usuario.setContrasena(contrasena);
+                    String curp = generarCurp(nombre, apellidoP, apellidoM, fechaNacimiento, genero, estado);
+                    usuario.setCurp(curp);
+                }
             }
-        }
-        if (!band) {
-            System.out.println("\nEste número de control no pertenece a ningún alumno");
+            if (!band) {
+                System.out.println("\nEste número de control no pertenece a ningún alumno");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error al modificar al alumno. Por favor, intente de nuevo.");
         }
     }
 }
